5371c8b33a01024368f6b56abefcc281b707e8d1
manojlamichhane <lammichhanemanoj1@gmail.com>
2023-08-15 16:42:37 -0400

    authOptions related issues

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   session: {
     strategy: "jwt",
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import { Footer, Navbar } from "@/components";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { store } from "./Redux/store";
-import { Provider } from "react-redux";
 import NextAuthProvider from "./NextAuthProvider";
 import { Providers } from "./Redux/Providers";
 
